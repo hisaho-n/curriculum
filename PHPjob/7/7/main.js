93cf1832c3d6eeadb10148271b5cc00b2aea2e8c
hisaho-n <hisaho_07@yahoo.co.jp>
2023-01-13 14:23:26 +0900

    7チェックテスト修正
    7チェックテスト修正
    問い１の修正

--- PHPjob/7/7/main.js
+++ PHPjob/7/7/main.js
@@ -2,8 +2,8 @@
 let numbers = [2, 5, 12, 13, 15, 18, 22];
 
 for (let i = 0; i < numbers.length; i++) {
-    if(i % 2 == 0){
-        isEven(i);
+    if(numbers[i] % 2 == 0){
+        isEven(numbers[i]);
     }
 }
 

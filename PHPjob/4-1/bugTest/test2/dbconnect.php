1d98d6c524097dba6f6f61fb1c2be66615ecade1
hisaho-n <hisaho_07@yahoo.co.jp>
2023-03-18 13:58:03 +0900

    ４章バグテスト・チェックテスト
    ４章バグテスト・チェックテスト

---
+++ PHPjob/4-1/bugTest/test2/dbconnect.php
@@ -0,0 +1,23 @@
+<?php
+// セッション開始
+session_start();
+// DBサーバのURL
+$db['host'] = "localhost";
+// ユーザー名
+$db['user'] = "root";
+// ユーザー名のパスワード
+$db['pass'] = "root";
+// データベース名
+$db['dbname'] = "YIGroupBlog";
+
+/* <?php
+// セッション開始
+session_start();
+// DBサーバのURL
+$db['host'] = "localhost";
+// ユーザー名
+$db['user'] = "root";
+// ユーザー名のパスワード
+$db['pass'] = "root";
+// データベース名
+$db['dbname'] = "loginManagement"; */
